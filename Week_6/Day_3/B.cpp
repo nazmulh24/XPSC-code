16ffd08e52fb162481880464dea44372307bd9a6
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2024-12-03 23:11:25 +0600

    Today I solve 2 problems

---
+++ Week_6/Day_3/B.cpp
@@ -0,0 +1,61 @@
+/***********************************************************************
+
+Name --> B - Phone Desktop
+Link --> https://vjudge.net/contest/676796#problem/B
+
+Example-->
+
+
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+
+    int ans = ceil(y / 2.0);
+    int rem = (y / 2) * 7 + (y % 2) * 11;
+    int xx = max(0LL, (x - rem));
+
+    ans += ceil(xx / 15.0);
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t; // Uncomment if multiple test cases
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Week_6/Day_3/E.cpp
@@ -0,0 +1,91 @@
+/***********************************************************************
+
+Name --> E - Mark the Dust Sweeper
+Link --> https://vjudge.net/contest/676796#problem/E
+
+Example-->
+
+
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    trav(A, a) cin >> A;
+
+    reverse(all(a));
+
+    rfor(i, n - 1, 0)
+    {
+        if (a[i] != 0)
+            break;
+
+        a.pop_back();
+    }
+
+    // trav(A, a) cout<< A<<" ";
+    // cout<<endl;
+
+    if (a.empty())
+    {
+        cout << 0 << endl;
+        return;
+    }
+
+    reverse(all(a));
+
+    n = sz(a);
+
+    int ans = 0;
+
+    forl(i, 0, n - 1)
+    {
+        if (a[i] == 0)
+            ans++;
+        else
+            ans += a[i];
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t; // Uncomment if multiple test cases
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
