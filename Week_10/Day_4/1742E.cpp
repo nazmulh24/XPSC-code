cd3ecdf987c67c8604f719eecd3325d6a95f273f
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-02 23:06:01 +0600

    Today I solved 6 problems

---
+++ Week_10/Day_4/1742E.cpp
@@ -0,0 +1,116 @@
+/***********************************************************************
+
+Name --> E. Scuza
+Link --> https://codeforces.com/problemset/problem/1742/E
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n + 1), mxIdx(n + 1), pSum(n + 1);
+
+    forl(i, 1, n + 1)
+    {
+        cin >> a[i];
+
+        pSum[i] = pSum[i - 1] + a[i];
+        mxIdx[i] = max(mxIdx[i - 1], a[i]);
+    }
+
+    while (q--)
+    {
+        int x;
+        cin >> x;
+
+        auto it = upper_bound(all(mxIdx), x);
+        int k = it - mxIdx.begin() - 1;
+
+        if (k >= 0)
+            cout << pSum[k] << " ";
+        else
+            cout << 0 << " ";
+    }
+
+    cout << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Week_10/Day_4/D.cpp
@@ -0,0 +1,76 @@
+/***********************************************************************
+
+Name --> D - Subarray Sums II
+Link --> https://vjudge.net/contest/682611#problem/D
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> a(n);
+    trav(A, a) cin >> A;
+
+    int sum = 0;
+    map<int, int> m;
+
+    m[0]++;
+
+    int ans = 0;
+
+    forl(i, 0, n)
+    {
+        sum += a[i];
+
+        ans += m[sum - x];
+        // cout << ans << " ";
+
+        m[sum]++;
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    // cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
