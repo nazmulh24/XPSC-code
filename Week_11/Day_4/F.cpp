e3f233f7f0ceb905b7d8ba38efe6cddc2f89c7fc
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-09 22:32:11 +0600

    Today I solved 3 problems

---
+++ Week_11/Day_4/F.cpp
@@ -0,0 +1,116 @@
+/***********************************************************************
+
+Name --> F - Klee's SUPER DUPER LARGE Array!!!
+Link --> https://vjudge.net/contest/683822#problem/F
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    auto ok = [](int kk, int nn)
+    {
+        int sum = (nn * (kk + nn - 1 + kk)) / 2;
+
+        return sum;
+    };
+
+    int l = 0, r = n - 1, mid, ans = INT_MAX;
+
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+
+        int F_half = ok(k, mid);
+        int S_half = ok(k + mid, n - mid);
+
+        ans = min(ans, abs(F_half - S_half));
+
+        if (S_half > F_half)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
