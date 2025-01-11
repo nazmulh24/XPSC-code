435765a64785675993dfc450d3a12c999221d965
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-11 22:50:51 +0600

    today I solved 2 problems

---
+++ Week_11/Day_5/G.cpp
@@ -0,0 +1,126 @@
+/***********************************************************************
+
+Name --> G - Building an Aquarium
+Link --> https://vjudge.net/contest/683822#problem/G
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int n, h;
+    cin >> n >> h;
+
+    vector<int> a(n);
+    trav(A, a) cin >> A;
+
+    auto ok = [&](int mid)
+    {
+        int w_need = 0;
+
+        forl(i, 0, n)
+        {
+            if (mid > a[i])
+                w_need += (mid - a[i]);
+
+            if (w_need > h)
+                return false;
+        }
+
+        return w_need <= h;
+    };
+
+    int l = 0, r = 2e9, mid, ans = 1;
+
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+
+        if (ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+            r = mid - 1;
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
---
+++ Week_11/Day_5/H.cpp
@@ -0,0 +1,130 @@
+/***********************************************************************
+
+Name --> H - Save More Mice
+Link --> https://vjudge.net/contest/683822#problem/H
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(k);
+    trav(A, a) cin >> A;
+
+    sort(all(a));
+
+    auto ok = [&](int mid)
+    {
+        int c = 0;
+
+        rfor(i, (k - 1), (k - mid))
+        {
+            int x = a[i];
+
+            if (x <= c)
+                return false;
+
+            int d = n - x;
+            c += d;
+        }
+
+        return true;
+    };
+
+    int l = 0, r = k, mid, ans;
+
+    while (l <= r)
+    {
+        mid = (l + r) / 2;
+
+        if (ok(mid))
+        {
+            ans = mid;
+            l = mid + 1;
+        }
+        else
+            r = mid - 1;
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
