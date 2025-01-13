f9e1e234f5b5a913e39af0ac67718036d23a2e2a
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-13 23:30:39 +0600

    Today I solved 2 problems

---
+++ Week_11/Day_6/J.cpp
@@ -0,0 +1,128 @@
+/***********************************************************************
+
+Name --> J - Final Boss
+Link --> https://vjudge.net/contest/683822#problem/J
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int h, n;
+    cin >> h >> n;
+
+    vector<int> a(n), c(n);
+    trav(A, a) cin >> A;
+    trav(C, c) cin >> C;
+
+    auto ok = [&](int mid)
+    {
+        int dmg = 0;
+
+        forl(i, 0, n)
+        {
+            int x = (mid + c[i] - 1) / c[i];
+
+            dmg += (x * a[i]);
+
+            if (dmg >= h)
+                return true;
+        }
+
+        return dmg >= h;
+    };
+
+    int l = 1, r = 1e12, mid, ans;
+
+    while (l <= r)
+    {
+        mid = l + (r - l) / 2;
+
+        if (ok(mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
