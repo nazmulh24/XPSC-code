8f4c8d31fb777e50061eea4df1158cff509b8886
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-14 23:34:32 +0600

    Today I solved 3 problems

---
+++ Week_12/Day_1/C.cpp
@@ -0,0 +1,107 @@
+/***********************************************************************
+
+Name --> C - T-primes
+Link --> https://vjudge.net/contest/685500#problem/C
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+bool Perfect_Sqr(int n)
+{
+    int x = sqrtl(n);
+
+    return (x * x) == n;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    forl(i, 0, n)
+    {
+        int x;
+        cin >> x;
+        if (Perfect_Sqr(x) and isPrime(sqrtl(x)))
+            yes;
+        else
+            no;
+    }
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    // cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
