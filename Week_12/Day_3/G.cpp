a65311cf8e8ec0b2409aa165c9ef78c62365b3f8
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-01-16 23:30:49 +0600

    Today I solved 1 problem

---
+++ Week_12/Day_3/G.cpp
@@ -0,0 +1,121 @@
+/***********************************************************************
+
+Name --> B. Different Divisors
+Link --> https://codeforces.com/problemset/problem/1474/B
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define rfor(i, b, a) for (int i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+int fPrime(int x)
+{
+    int i = x;
+
+    while (1)
+    {
+        bool ok = true;
+
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok)
+            return i;
+
+        i++;
+    }
+
+    return 0;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int p1 = fPrime(1 + n);
+    int p2 = fPrime(p1 + n);
+
+    cout << min((p1 * p2), (p1 * p1 * p1)) << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
