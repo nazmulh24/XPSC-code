c5f6b1ca4cdb6cfd9044d27e305e9a4b3e14fc00
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-02-15 23:10:56 +0600

    Today I solved 3 problems

---
+++ Week_15/Day_3/C.cpp
@@ -0,0 +1,118 @@
+/***********************************************************************
+
+Name --> C - Raspberries
+Link --> https://vjudge.net/contest/691268#problem/C
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (auto i = (a); i <= (b); ++i)
+#define rfor(i, b, a) for (auto i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int evn = 0, ans = k;
+
+    forl(i, 0, n - 1)
+    {
+        int x;
+        cin >> x;
+
+        if (x % 2 == 0)
+            evn++;
+
+        if (x % k == 0)
+            ans = 0;
+
+        ans = min(ans, k - x % k);
+    }
+
+    if (k == 4)
+    {
+        if (evn >= 2)
+            ans = 0;
+        else if (evn == 1)
+            ans = min(ans, 1ll);
+        else
+            ans = min(ans, 2ll);
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
