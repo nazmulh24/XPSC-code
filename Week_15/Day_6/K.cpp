da6e9ec35e28f7ab26b3f190d28b89e2e28b9a02
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-02-19 19:46:23 +0600

    Today I solved 3 problems

---
+++ Week_15/Day_6/K.cpp
@@ -0,0 +1,129 @@
+/***********************************************************************
+
+Name --> K - Tear It Apart
+Link --> https://vjudge.net/contest/691268#problem/K
+
+Example-->
+
+------------------------------------------------------------------------
+Author --> Nazmul Hossain
+***********************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define forl(i, a, b) for (auto i = (a); i <= (b); ++i)
+#define rfor(i, b, a) for (auto i = (b); i >= (a); --i)
+#define trav(a, x) for (auto &a : x)
+#define UNIQUE(v) v.erase(unique(all(v)), v.end())
+//********************************************************************
+
+bool isPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int GCD(int a, int b)
+{
+    return __gcd(a, b); //--> O(log(min(a, b)))
+}
+
+int LCM(int a, int b)
+{
+    return (a / __gcd(a, b)) * b; //--> O(log(min(a, b)))
+}
+
+void PSUM(vector<int> a, vector<int> &pSum)
+{
+    int n = sz(a);
+    pSum[0] = a[0];
+    forl(i, 1, n)
+    {
+        pSum[i] = pSum[i - 1] + a[i];
+    }
+}
+
+//********************************************************************
+
+int check(int x)
+{
+    int cnt = 0;
+
+    while (x > 0)
+    {
+        x = x / 2;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    int n = sz(s);
+    int ans = n;
+
+    forl(i, 0, 25)
+    {
+        char c = 'a' + i;
+
+        int l = 0, mx = 0;
+
+        forl(j, 0, n - 1)
+        {
+            if (s[j] == c)
+            {
+                mx = max(mx, l);
+                l = 0;
+            }
+            else
+                l++;
+        }
+
+        mx = max(mx, l);
+        ans = min(ans, mx);
+    }
+
+    cout << check(ans) << endl;
+}
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int ttttt = 1;
+    cin >> ttttt; //--> Uncomment if multiple test cases
+
+    while (ttttt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
